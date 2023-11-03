cd1e8cfb7f6bdd976bf7192ec577f94ad82824a1
Aldwin <91401877+goopiktu@users.noreply.github.com>
2023-11-03 10:15:45 +0800

    Add: Able to see the all the orders ToDo: button that accepts, rejects

--- admin/frontend/src/App.js
+++ admin/frontend/src/App.js
@@ -2,22 +2,32 @@ import { useEffect, useState } from "react";
 import useFetch from "./hooks/useFetch";
 
 function App() {
-  const { data: Order, loading } = useFetch("/api/orders");
-  const [firstName, setFirstName] = useState('');
+  const { data: orders, loading } = useFetch("/api/orders");
 
   useEffect(() => {
-    if (Order && Order.fname) {
-      setFirstName(Order.fname);
-    }
-    console.log(Order.fname)
-  }, [Order]);
+    console.log(orders);
+  }, [orders]);
 
   return (
     <div>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <p>{Order.fname}</p>
+        <div>
+          <h2>All Orders</h2>
+          <ul>
+            {orders.map((order, index) => (
+              <li key={index}>
+                <p>Order {index + 1}:</p>
+                <p>First Name: {order.fname}</p>
+                <p>Last Name: {order.lname}</p>
+                <p>Contact Number: {order.contactNo}</p>
+                <p>Mode: {order.mode}</p>
+                <p>Date Ordered: {order.dateOrdered}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
